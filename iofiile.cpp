29a2c3c830e84a94ed2abe7b91e2d8c7de1b9aaa
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-06-04 14:49:24 +0700

    membuat logic untuk file membaca

--- iofiile.cpp
+++ iofiile.cpp
@@ -24,5 +24,26 @@ while(true) {
     //menulis dan memasukan nilai dari ''baris ke dalam file
     outfile << baris << endl;
  }
+ //selesai dalam menulis sekarang tutup file nya
+ outfile.close();
+
+ //membuka file dalam mode membaca
+ ifstream infile;
+ //menunjuk ke sebuah file
+ infile.open("contohfile.txt");
+
+ cout << endl << ">= Membuka dan membaca file " << endl;
+ //jika file ada maka
+ if (infile.is.open())
+{
+    //melakukan perulangan setiap baris
+    while(getline(infile, baris))
+    {
+        //dan tampilkan di sini
+        cout << baris << '\n';
+    }
+    //tutup file tersebut setelah selesai
+    infile.close();
+}
 }
 
