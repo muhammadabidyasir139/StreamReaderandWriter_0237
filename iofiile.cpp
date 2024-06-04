8eb50efb4bd5aa03b38343b6910c9e867fd81c0d
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-06-04 14:34:29 +0700

    membuat code untuk file dalam kondisi meulis

--- iofiile.cpp
+++ iofiile.cpp
@@ -3,3 +3,14 @@
 #include <string>
 
 using namespace std;
+
+int main() {
+    string baris;
+
+    //membuka file dalam kode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+}
